436d7eddac79e57b78ea4ab9529dcf2ffb1f9bfe
yeedle <abeneuwirth@gmail.com>
2017-01-02 18:59:21 -0500

    cleaned up code in init

--- init.R
+++ init.R
@@ -7,19 +7,30 @@ library(dplyr)
 library(stringr)
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-term <- "barack obama"
-result <- searchTwitter(term, 1000, lang = "en", resultType = "recent")
-regex <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|\\.?@[A-Za-z0-9_]+|&amp;|&lt;|&gt;|RT|rt|https|t.co"
 nrc <- get_sentiments("nrc")
 bing <- get_sentiments("bing")
 afinn <- get_sentiments("afinn")
+
+
+n <- 1000
+language <- "en"
+result_type <- "recent"
+term <- "barack obama"
+result <- searchTwitter(term, n, lang = language, resultType = result_type)
+
+regex <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|\\.?@[A-Za-z0-9_]+|&amp;|&lt;|&gt;|RT|rt|https|t.co"
+
 tweets <-  result %>%
   map_df(as.data.frame) %>% 
-  tbl_df() %>%
+  tbl_df()
+  
+tweets <- tweets %>%
   mutate(text = str_replace_all(text, regex, "")) %>%
   mutate(text = str_replace_all(text, coll(term, ignore_case = T), "")) %>%
   unnest_tokens(output = token, input = text) %>% 
-  dplyr::filter(!token %in% stop_words$word) %>% 
+  dplyr::filter(!token %in% stop_words$word)
+  
+tweets <- tweets %>%
   left_join(bing, by = c("token" = "word")) %>% 
   group_by(sentiment) %>%
   summarize(count = n()) %>%
