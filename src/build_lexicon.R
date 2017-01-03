c6384129d7f818b2b46b1194c0d3629816ba187b
yeedle <abeneuwirth@gmail.com>
2017-01-03 13:02:40 -0500

    script to build combined lexicon

---
+++ src/build_lexicon.R
@@ -0,0 +1,36 @@
+library(twitteR)
+library(dplyr)
+
+afinn <- get_sentiments("afinn")
+nrc <- get_sentiments("nrc")
+bing <- get_sentiments("bing")
+
+augmented_afinn <- afinn %>% mutate(sentiment = 
+                                      case_when(.$score < 0 ~ "negative",
+                                                .$score > 0 ~ "positive"))
+nrc_sentiments <- nrc %>% filter(sentiment %in% c("positive", "negative"))
+
+disputes <- full_join(augmented_afinn, bing, by=c("word"="word")) %>% 
+  full_join(nrc_sentiments, by=c("word"="word")) %>%
+  filter(sentiment.x != sentiment.y | 
+           sentiment.y != sentiment | 
+           sentiment.x != sentiment) 
+
+resolved_disputes <- disputes %>%
+  mutate(afinn = case_when(.$sentiment.x == "positive" ~ 1,
+                           .$sentiment.x == "negative" ~ -1),
+         bing = case_when(.$sentiment.y == "positive" ~ 1,
+                          .$sentiment.y == "negative" ~ -1),
+         nrc = case_when(.$sentiment == "positive" ~ 1,
+                         .$sentiment == "negative" ~ -1),
+         consensus = afinn + bing + nrc) %>%
+  filter(!is.na(consensus)) %>%
+  mutate(sentiment = ifelse(consensus == 1, "positive","negative")) %>%
+  select(word, sentiment)
+
+combined <- full_join(augmented_afinn, bing, by = c("word", "sentiment")) %>% 
+  full_join(nrc_sentiments, by = c("word", "sentiment")) %>%
+  select(word, sentiment) %>% 
+  filter(word %notin% disputes$word) %>%
+  full_join(resolved_disputes, by = c("word", "sentiment"))
+
--- src/init.R
+++ src/init.R
@@ -5,6 +5,8 @@ library(tidytext)
 library(purrr)
 library(dplyr)
 library(stringr)
+
+options(httr_oauth_cache = TRUE) 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
 nrc <- get_sentiments("nrc")
@@ -15,26 +17,28 @@ afinn <- get_sentiments("afinn")
 n <- 1000
 language <- "en"
 result_type <- "recent"
-term <- "barack obama"
-result <- searchTwitter(term, n, lang = language, resultType = result_type)
+
+term <- "hitler"
+
+result <- searchTwitter(term, n, lang = language, resultType = result_type) %>%
+  map_df(as.data.frame) %>% 
+  tbl_df()
 
 regex <- "https://t.co/[A-Za-z\\d]+|http://[A-Za-z\\d]+|\\.?@[A-Za-z0-9_]+|&amp;|&lt;|&gt;|RT|rt|https|t.co"
 
 tweets <-  result %>%
-  map_df(as.data.frame) %>% 
-  tbl_df()
-  
-tweets <- tweets %>%
   mutate(text = str_replace_all(text, regex, "")) %>%
   mutate(text = str_replace_all(text, coll(term, ignore_case = T), "")) %>%
   unnest_tokens(output = token, input = text) %>% 
-  dplyr::filter(!token %in% stop_words$word)
+  filter(!token %in% stop_words$word)
   
 tweets <- tweets %>%
-  left_join(bing, by = c("token" = "word")) %>% 
+  left_join(afinn, by = c("token" = "word")) %>% 
+  group_by(id) %>%
+  summarize(summed_score = sum(score,na.rm = T)) %>%
+  mutate(sentiment = if_else(summed_score < 0, "negative", if_else(summed_score == 0, "neutral", "positive"))) %>%
   group_by(sentiment) %>%
   summarize(count = n()) %>%
-  filter(!is.na(sentiment)) %>%
   mutate(percentage = count/sum(count))
 
 
